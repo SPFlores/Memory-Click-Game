500a89122c9560937cd28ac5807c730ceae38276
SPFlores <sflores92886@gmail.com>
2019-06-24 19:17:57 -0700

    app set up

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css'
+
+// const pictures = []
+
+class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>hello there</h1>
+      </>
+    )
+  }
+}
+
+export default App
