b8468ce3b4ff150a8600b18b87639d7d3f7ef4df
SPFlores <sflores92886@gmail.com>
2019-06-25 21:11:05 -0700

    clicking, ids, state variables

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,26 @@ import './App.css'
 import Nav from './Components/Nav'
 import Card from './Components/Card'
 
-const pictures = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
 class App extends Component {
   state = {
     currentScore: 0,
-    topScore: 0
+    topScore: 0,
+    hasbeenclicked: true,
+    hasnotbeenclicked: false,
+    pictures: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   }
 
-  handleClick = event => {
-    console.log(event)
+  handleClick = e => {
+    console.log(e.target)
   }
 
   render() {
     return (
       <>
         <Nav currentScore={this.state.currentScore} topScore={this.state.topScore} />
-        <Card handleClick={this.handleClick} pictures={pictures}/>
+        <p>Something here about what you should do for the game.</p>
+        <Card handleClick={this.handleClick} pictures={this.state.pictures} clicked={this.state.hasnotbeenclicked} />
+        {/* find the randomize function on the NSTAAF HW */}
       </>
     )
   }
--- client/src/Components/Card/card.js
+++ client/src/Components/Card/card.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react'
 import './card.css'
 
 class Card extends Component {
+  // componentDidMount = _ => {
+  //   console.log(this.props)
+  // }
+
   render() {
     return (
       <>
         {
           this.props.pictures.map(picture => {
+            console.log(this.props)
             return (
-              <div className='card' onClick={this.props.handleClick}>
-                <h1>{picture}</h1>
+              <div className='card' onClick={this.props.handleClick} key={picture} id={this.props.clicked} >
+                {picture}
               </div>
             )
           })
