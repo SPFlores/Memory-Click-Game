5c1300d59c0a8758da251cdfc11acae8fec7ea84
SPFlores <sflores92886@gmail.com>
2019-06-26 20:21:43 -0700

    read click and check if clicked

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,57 @@ class App extends Component {
   state = {
     currentScore: 0,
     topScore: 0,
-    hasbeenclicked: true,
-    hasnotbeenclicked: false,
-    pictures: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    pictures: [
+      {
+        number: 1,
+        link: 'something 1'
+      }, {
+        number: 2,
+        link: 'something 2'
+      }, {
+        number: 3,
+        link: 'something 3'
+      }, {
+        number: 4,
+        link: 'something 4'
+      }, {
+        number: 5,
+        link: 'something 5'
+      }, {
+        number: 6,
+        link: 'something 6'
+      }, {
+        number: 7,
+        link: 'something 7'
+      }, {
+        number: 8,
+        link: 'something 8'
+      }, {
+        number: 9,
+        link: 'something 9'
+      }, {
+        number: 10,
+        link: 'something 10'
+      }
+    ],
+    hasBeenClicked: [],
+    cardNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   }
 
   handleClick = e => {
-    console.log(e.target)
+    // check if the id is in this.state.hasBeenClicked
+    if ((this.state.cardNumbers.indexOf(parseInt(e.target.id)) >= 0) && (this.state.hasBeenClicked.indexOf(parseInt(e.target.id)) < 0)) {
+      // if it isn't
+      let beenClicked = this.state.hasBeenClicked
+      beenClicked.push(parseInt(e.target.id))
+      this.setState({ hasBeenClicked: beenClicked })
+      console.log(this.state.hasBeenClicked)
+      // increment score (separate function?)
+    } else if ((this.state.hasBeenClicked.indexOf(parseInt(e.target.id)) >= 0) && (this.state.cardNumbers.indexOf(parseInt(e.target.id)) >= 0)) {
+      console.log('wrong')
+    }
+    // if it is
+    // reset hasBeenClicked, stop score increment, set highest score (separate func?)
   }
 
   render() {
@@ -21,7 +65,7 @@ class App extends Component {
       <>
         <Nav currentScore={this.state.currentScore} topScore={this.state.topScore} />
         <p>Something here about what you should do for the game.</p>
-        <Card handleClick={this.handleClick} pictures={this.state.pictures} clicked={this.state.hasnotbeenclicked} />
+        <Card handleClick={this.handleClick} pictures={this.state.pictures} />
         {/* find the randomize function on the NSTAAF HW */}
       </>
     )
--- client/src/Components/Card/card.js
+++ client/src/Components/Card/card.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 import './card.css'
 
 class Card extends Component {
-  // componentDidMount = _ => {
-  //   console.log(this.props)
-  // }
+  state = {
+
+  }
 
   render() {
     return (
       <>
         {
           this.props.pictures.map(picture => {
-            console.log(this.props)
+            // console.log(this.props)
             return (
-              <div className='card' onClick={this.props.handleClick} key={picture} id={this.props.clicked} >
-                {picture}
+              <div className='card' onClick={this.props.handleClick} key={picture.number} id={picture.number} >
+                {/*<h1 id={picture.number}>{picture.link}</h1>
+                <h2>{
+                  !this.props.clicked ? 'false' : 'true'
+                }</h2> */}
               </div>
             )
           })
